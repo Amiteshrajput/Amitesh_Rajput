6d59b5256e96458b74715957918ce84b85e20d9e
Devansh0309 <samdeva0309@gmail.com>
2023-01-15 12:48:47 +0530

    Created new branch

--- src/Auth/LogIn.jsx
+++ src/Auth/LogIn.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import {signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebaseConfig/firebaseConfig';
+import { getDoc, setDoc, doc } from "firebase/firestore";
+import { db } from '../firebaseConfig/firebaseConfig';
+import './LogIn.css'
 
 function LogIn({type}) {
-  return (
-    <div>LogIn</div>
+  const navigate=useNavigate()
+  const signIn=()=>{
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+    .then(async(result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    // const credential = GoogleAuthProvider.credentialFromResult(result);
+    // const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;//userThat is signin-ing  in
+    console.log(user)
+    
+    const docRef = doc(db, "adminData", user.uid);
+    const docSnap = await getDoc(docRef);
+    const userData=docSnap.data()//user on database like admin or premium user
+    console.log(type+''+JSON.stringify(userData))
+    if(user && userData && userData.email===user.email){
+      alert('Admin SignIn successfull')
+      navigate('/admin/profile')
+    }
+    else{
+      alert('Admin SignIn Unsuccessfull')
+      navigate('/')
+    }
+    // if(userData && userData.type===type){
+    //   //profile
+    // }
+    // else if(userData && userData.type!==type){
+    //   //invalid access  or invalid type user accessing
+    //   alert(`Invalid access as you are trying to signIn as ${type} but you onboarded as ${userData.type} using this email`)
+    // }
+    // else{
+    //   //onBoarding
+    //   console.log(type+''+userData)
+    // }
+  }).catch((error) => {
+    // // Handle Errors here.
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // // The email of the user's account used.
+    // const email = error.customData.email;
+    // // The AuthCredential type that was used.
+    // const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(error)
+  });
+  }
+  return(
+    <div className='auth-container'>
+      <h1>Welcome {type}</h1>
+      <h2>Sign In</h2>
+      <br/><br/>
+      <button type="button" onClick={signIn}>
+        <img src="https://shortcuts-france.fr/wp-content/uploads/2021/04/google-logo-carre-2015-09-400.png" alt=""/>
+        <h2>Sign in</h2>
+      </button>
+    </div>
   )
 }
-
 export default LogIn
--- src/Navs.jsx
+++ src/Navs.jsx
@@ -11,7 +11,7 @@ import LogIn from './Auth/LogIn';
 import PremiumUser from './Components/PremiumUser/PremiumUser';
 import LandingPage from './Components/LandingPage/LandingPage';
 import NavBar from './Components/Navbar';
-import AdminProfile from './Components/Admin/Admin';
+import AdminProfile from './Components/Admin/Profile/Admin';
 import AdminAccount from './Components/Admin/Account/AdminAccount';
 import AdminDashboard from './Components/Admin/Dashboard/AdminDashboard';
 
@@ -32,16 +32,21 @@ function Navs() {
     <BrowserRouter>
      <Routes>
         <Route path='/' element={<NavBar><br/><br/><br/><LandingPage/></NavBar>}/>
+
         <Route path='/admin/auth' element={<NavBar><br/><br/><br/><LogIn type='Admin'/></NavBar>}/>
+
         <Route path='/premiumUser/auth' element={<NavBar><br/><br/><br/><LogIn type='PremiumUser'/></NavBar>}/>
+
         <Route element={<AdminProtectedRoutes/>}>
-          <Route path='/admin/profile' element={<NavBar><br/><br/><br/><AdminProfile/></NavBar>}/>
+          <Route path='/admin/profile' element={<NavBar><br/><br/><br/><br/><AdminProfile/></NavBar>}/>
           <Route path='/admin/account' element={<NavBar><br/><br/><br/><AdminAccount/></NavBar>}/>
           <Route path='/admin/dashboard' element={<NavBar><br/><br/><br/><AdminDashboard/></NavBar>}/>
         </Route>
+
         <Route element={<PremiumUserProtectedRoutes/>}>
           <Route path='/premiumUser' element={<NavBar><br/><br/><br/><PremiumUser/></NavBar>}/>
         </Route>
+
      </Routes>
     </BrowserRouter>
   )
